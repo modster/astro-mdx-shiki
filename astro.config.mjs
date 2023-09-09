0ef5aca73bbdff9dc2f42a4bf033ddca61b0b091
E. M. Greeff <emgreeff@datadash.cloud>
2023-09-09 10:51:21 -0600

    biome config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx";
-import svelte from "@astrojs/svelte";
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import svelte from "@astrojs/svelte"
+import react from "@astrojs/react"
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Custom Markdown options, plugins, etc.
   markdown: {
-    syntaxHighlight: 'shiki',
+    syntaxHighlight: "shiki",
     shikiConfig: {
-      theme: 'dracula'
+      theme: "dracula",
       // Learn more about this configuration here:
       // https://docs.astro.build/en/guides/markdown-content/#syntax-highlighting
-    }
+    },
   },
 
-  integrations: [mdx(), svelte(), react(), tailwind()]
-});
+  integrations: [mdx(), svelte(), react(), tailwind()],
+})
---
+++ prettier.config.cjs
@@ -0,0 +1,35 @@
+// prettier.config.js
+module.exports = {
+    tailwindConfig: "./styles/tailwind.config.js",
+    printWidth: 120,
+    tabWidth: 4,
+    singleAttributePerLine: false,
+    semi: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    trailingComma: "all",
+    proseWrap: "preserve",
+    embeddedLanguageFormatting: "auto",
+}
+
+// prettier.arrowParens
+// prettier.bracketSpacing
+// prettier.endOfLine
+// prettier.htmlWhitespaceSensitivity
+// prettier.insertPragma
+// prettier.singleAttributePerLine
+// prettier.bracketSameLine
+// prettier.jsxBracketSameLine
+// prettier.jsxSingleQuote
+// prettier.printWidth
+// prettier.proseWrap
+// prettier.quoteProps
+// prettier.requirePragma
+// prettier.semi
+// prettier.singleQuote
+// prettier.tabWidth
+// prettier.trailingComma
+// prettier.useTabs
+// prettier.vueIndentScriptAndStyle
+// prettier
