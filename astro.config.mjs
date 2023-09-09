7a641516dd5f89d81ea06156b055e6fa25f69d4e
E. M. Greeff <emgreeff@datadash.cloud>
2023-09-09 15:39:45 -0600

    deploy to staging

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import svelte from "@astrojs/svelte"
-import react from "@astrojs/react"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
 
-import tailwind from "@astrojs/tailwind"
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,11 +11,11 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
-      theme: "dracula",
+      theme: "nord",
       // Learn more about this configuration here:
       // https://docs.astro.build/en/guides/markdown-content/#syntax-highlighting
     },
   },
 
   integrations: [mdx(), svelte(), react(), tailwind()],
-})
+});
--- src/components/StarField.jsx
+++ src/components/StarField.jsx
@@ -1,15 +1,15 @@
-import { Canvas } from "@react-three/fiber"
-import { Stars, PerspectiveCamera } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber";
+import { Stars, PerspectiveCamera } from "@react-three/drei";
 
 function StarField() {
-    return (
-        <>
-            <Canvas>
-                <PerspectiveCamera makeDefault position={[0, 0, 100]} />
-                <Stars radius={100} depth={50} count={5000} factor={7} saturation={3} fade speed={0.1} />
-            </Canvas>
-        </>
-    )
+  return (
+    <>
+      <Canvas>
+        <PerspectiveCamera makeDefault position={[0, 0, 100]} />
+        <Stars radius={100} depth={50} count={5000} factor={7} saturation={3} fade speed={0.1} />
+      </Canvas>
+    </>
+  );
 }
 
-export default StarField
+export default StarField;
