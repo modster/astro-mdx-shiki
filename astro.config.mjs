1c04268201ed15825d77ddadf413bb807e0d2ead
E. M. Greeff <emgreeff@datadash.cloud>
2023-09-10 04:32:24 -0600

    Squashed commit of the following:
    commit f10a672e81bcb55686132162985cbf80b03a9288
    Author: E. M. Greeff <emgreeff@datadash.cloud>
    Date:   Sat Sep 9 16:41:23 2023 -0600
    
        host on ubuntu
    
    commit 7a641516dd5f89d81ea06156b055e6fa25f69d4e
    Author: E. M. Greeff <emgreeff@datadash.cloud>
    Date:   Sat Sep 9 15:39:45 2023 -0600
    
        deploy to staging
    
    commit 0ef5aca73bbdff9dc2f42a4bf033ddca61b0b091
    Author: E. M. Greeff <emgreeff@datadash.cloud>
    Date:   Sat Sep 9 10:51:21 2023 -0600
    
        biome config
    
    commit da09d0850b54a8be25e68530702fd93b67c76cc2
    Author: E. M. Greeff <emgreeff@datadash.cloud>
    Date:   Sat Sep 9 09:13:26 2023 -0600
    
        mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
@@ -9,13 +9,13 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   // Enable Custom Markdown options, plugins, etc.
   markdown: {
-    syntaxHighlight: 'shiki',
+    syntaxHighlight: "shiki",
     shikiConfig: {
-      theme: 'dracula'
+      theme: "nord",
       // Learn more about this configuration here:
       // https://docs.astro.build/en/guides/markdown-content/#syntax-highlighting
-    }
+    },
   },
-
-  integrations: [mdx(), svelte(), react(), tailwind()]
+  server: { port: 8000, host: true},
+  integrations: [mdx(), svelte(), react(), tailwind()],
 });
